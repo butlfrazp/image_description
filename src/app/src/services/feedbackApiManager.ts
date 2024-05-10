eeda8a4e0063685288b5835d09cd9d21e33f4e98
Paul Butler <paulfbutler2016@gmail.com>
2024-05-10 09:20:43 -0700

    update to include build files

--- src/app/src/services/feedbackApiManager.ts
+++ src/app/src/services/feedbackApiManager.ts
@@ -5,12 +5,13 @@ import { FeedbackItem } from '../models/feedbackItem';
 const _BASE_URL = process.env.REACT_APP_API_URL || '';
 
 export const getFeedbackIds = async (): Promise<string[]> => {
-    const response = await axios.get(`${_BASE_URL}/api/feedback/ids`);
-    return response.data as string[];
+    const response = await axios.get(`${_BASE_URL}/v1/feedback/documents?select=id`);
+    const data = response.data as { id: string }[];
+    return data.map((item) => item.id);
 };
 
 export const getFeedback = async (id: string) => {
-    const response = await axios.get(`${_BASE_URL}/api/feedback/${id}`);
+    const response = await axios.get(`${_BASE_URL}/v1/feedback/documents/${id}`);
     return response.data as FeedbackItem;
 };
 
@@ -19,5 +20,5 @@ export const uploadFeedback = async (sessionId: string, id: string, feedback: st
         "session-id": sessionId,
     };
 
-    await axios.post(`${_BASE_URL}/api/feedback/${id}`, { feedback, rating }, { headers: headers });
+    await axios.post(`${_BASE_URL}/api/feedback/documents/${id}/feedback`, { feedback, rating }, { headers: headers });
 };
